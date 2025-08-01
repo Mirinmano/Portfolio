19b0be4f94f89ecad53bf21f09751129a2765a12
Mirin Mano M <717822y129@kce.ac.in>
2025-08-01 21:07:22 +0530

    portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      // Add this to make sure the service worker is included
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Mirin Mano M',
-        short_name: 'Profile',
-        description: 'Mirinmano Portfolio',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '1000078290.jpg',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '1000078290.jpg',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: '1000078290.jpg',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [tailwindcss(), react()],
 })
